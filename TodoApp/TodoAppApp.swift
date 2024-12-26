2fe5fa8005b5bd97cf0f0ffc8b90b2d9e7f95488
Your GitHub Username <your-email@example.com>
2024-12-26 16:53:15 +0800

    Initial Commit

---
+++ TodoApp/TodoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoAppApp.swift
+//  TodoApp
+//
+//  Created by Jinbo Zhang on 2024/12/26.
+//
+
+import SwiftUI
+
+@main
+struct TodoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
